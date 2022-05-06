8fd5fcc4eb9e67d0e8c8e3f664eb349deed94843
KainanGB <kainanytbr@gmail.com>
2022-05-06 13:44:33 -0300

    started doing the CSS Layout with SASS Almost finishing API call logic

--- images/images.js
+++ images/images.js
@@ -1,10 +1,13 @@
 export const clouds =
-  "https://www.freepik.com/free-photo/blue-sky-with-cloud-closeup_6133060.htm";
+  "https://img.freepik.com/free-photo/blue-sky-with-cloud-closeup_150588-129.jpg?w=740&t=st=1651841988~exp=1651842588~hmac=34df32f9e08deb59a06c27d96b790bc5ebec30a6871ab1c84c798f402b68bf93";
 export const snow =
-  "https://www.freepik.com/free-photo/blue-sky-with-cloud-closeup_6133060.htm";
+  "https://img.freepik.com/free-photo/beautiful-shot-mountains-covered-with-snow-during-daytime_181624-45185.jpg?t=st=1651842034~exp=1651842634~hmac=5f145c6688c625dc228f2de7a9e94a583f3ca500b76af74a8e8acb483e41b392&w=740";
 export const thunder =
-  "https://www.freepik.com/free-photo/vertical-shot-lightning-hitting-tree-night-with-purple-sky-trees-front_10303514.htm";
+  "https://img.freepik.com/free-photo/vertical-shot-lightning-hitting-tree-night-with-purple-sky-trees-front_181624-15404.jpg?w=360&t=st=1651842058~exp=1651842658~hmac=3b8f80c1c414e78c73deb21fe6b84009916ea429254248e4ce12b83a61149911";
 export const rain =
-  "https://www.freepik.com/premium-photo/rain-drop-falling-onto-blackboard-with-green-nature-background_2320463.htm";
+  "https://img.freepik.com/free-photo/rain-drop-falling-onto-blackboard-with-green-nature-background_34152-328.jpg?w=740";
 export const fog =
-  "https://www.freepik.com/free-photo/high-angle-shot-beautiful-forest-with-lot-green-trees-enveloped-fog-new-zealand_10835665.htm";
+  "https://img.freepik.com/free-photo/high-angle-shot-beautiful-forest-with-lot-green-trees-enveloped-fog-new-zealand_181624-19717.jpg?w=740&t=st=1651842087~exp=1651842687~hmac=3578468c315251c9a0cb5a03537c6c03060c3bb7a726562d91e0fe79a3728c35";
+
+export const clean =
+  "https://img.freepik.com/fotos-gratis/ceu-azul-com-nuvens-brancas-inchadas_1385-12.jpg?w=826&t=st=1651855193~exp=1651855793~hmac=e97e3528f47004b50cc1cf41ef5c312aeabe8ba99c448a34e9e63d86642b2fb4";
--- index.js
+++ index.js
@@ -1,42 +1,55 @@
 import config from "./config.json" assert { type: "json" };
+import { clouds, snow, thunder, rain, fog, clean } from "./images/images.js";
 const { KEY } = config;
 
-import { clouds, snow, thunder, rain, fog } from "./images/images.js";
-
-const searchBtn = document.querySelector(".send");
-
+const card = document.querySelector(".card");
 const api = (city) => {
-  return `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${KEY}&lang=pt`;
+  return `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${KEY}&lang=pt_br&units=metric`;
 };
 
-const fetchWeather = async () => {
-  const response = await fetch(api("curitiba")).then((res) => res.json());
+const fetchWeather = async (value) => {
+  const response = await fetch(api(value)).then((res) => res.json());
 
   const { main, name, weather, wind } = response;
 
-  console.log(main);
-  //console.log(name);
-  //console.log(wind);
+  let tempo = weather[0].main;
   let currentWeatherImage;
-  if (weather[0].main === "Clouds") {
-    weather[0].main = "Nublado";
+
+  if (tempo === "Clouds") {
+    tempo = "Nublado";
     currentWeatherImage = clouds;
+  }
+  if (tempo === "Clear") {
+    tempo = "Limpo";
+    currentWeatherImage = clean;
   }
 
   let card = document.querySelector(".card");
-  if (weather.main === "Clouds") weather.main = "Nublado";
+  card.classList.add("active");
   card.innerHTML = `
     <div class="card__header">
-      <p class="">Cidade: ${name}</p>
-      <img src="${currentWeatherImage}" alt="">
-
+    <img src="${currentWeatherImage}" alt="">
     </div>
     <div class="card__body">
-      <p>Tempo:${weather[0].main} | ${weather[0].description}</p>
+    <p class="">${name}</p>
+      <p>Tempo: ${main.temp}ºC</p>
+      <p>${tempo}</p>
+      <p></p>
+      <p>Sensação térmica: ${main.feels_like}ºC </p>
       <p>Umidade: ${main.humidity}%</p>
+
+      <div class="buttons">
+      <input type="text" class="search-input" id="search" placeholder="City Name"/>
+      <button class="search-btn fa-solid fa-magnifying-glass"></button>
+    </div>
     </div>
   
   `;
 };
 
-searchBtn.addEventListener("click", fetchWeather);
+card.addEventListener("click", (event) => {
+  if (event.target.className === "search-btn fa-solid fa-magnifying-glass") {
+    let value = document.querySelector(".search-input").value;
+    fetchWeather(value);
+  }
+});
